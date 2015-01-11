6266049a354d629b4767f052606be6a671814a40
syriusz <syriusz.p@gmail.com>
2015-01-11 21:09:20 +0100

    TDD infra added

--- meadowlark.js
+++ meadowlark.js
@@ -3,14 +3,6 @@ var fortune = require('./lib/fortune.js');
 
 var app = express();
 
-var fortunes = [
-	"Conquer your fears or they will conquer you.",
-	"Rivers need springs.",
-	"Do not fear what you don't know.",
-	"You will have a pleasant surprise.",
-	"Whenever possible, keep it simple.",
-];
-
 // set up handlebars view engine
 var handlebars = require('express3-handlebars')
 	.create({ defaultLayout:'main' });
@@ -22,14 +14,35 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(req, res, next){
+	res.locals.showTests = app.get('env') !== 'production' &&
+		req.query.test === '1';
+	next();
+});
+
+
 app.get('/', function(req, res) {
 	res.render('home');
 });
 
 app.get('/about', function(req, res){
-	res.render('about', { fortune: fortune.getFortune() });
+	res.render('about', { 
+		fortune: fortune.getFortune(),
+		pageTestScript: '/qa/tests-about.js' 
+	});
 });
 
+app.get('/tours/hood-river', function(req, res){
+	res.render('tours/hood-river');
+});
+
+app.get('/tours/oregon-coast', function(req, res){
+	res.render('tours/oregon-coast');
+});
+
+app.get('/tours/request-group-rate', function(req, res){
+	res.render('tours/request-group-rate');
+});
 
 // 404 catch-all handler (middleware)
 app.use(function(req, res, next){
